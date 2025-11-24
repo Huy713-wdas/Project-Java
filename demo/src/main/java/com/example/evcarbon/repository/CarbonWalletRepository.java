c4cea942955f3489152a171665670a796bc72c7b
trong1605 <trongnl7145@ut.edu.vn>
2025-11-24 17:12:11 +0700

    Delete demo directory
    Delete

--- demo/src/main/java/com/example/evcarbon/repository/CarbonWalletRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.evcarbon.repository;
-
-
-import com.example.evcarbon.model.CarbonWallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CarbonWalletRepository extends JpaRepository<CarbonWallet, Long> {
-}
