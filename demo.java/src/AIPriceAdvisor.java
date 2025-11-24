5d1ce13aa3769ef716a7b4d0c762b193d63b1e6c
trong1605 <trongnl7145@ut.edu.vn>
2025-11-24 17:12:56 +0700

    Delete demo.java directory

--- demo.java/src/AIPriceAdvisor.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-
-public class AIPriceAdvisor {
-    private Random random = new Random();
-    public double suggestPrice(double avgPrice) {
-        return avgPrice * (0.95 + 0.1 * random.nextDouble());
-    }
-}
