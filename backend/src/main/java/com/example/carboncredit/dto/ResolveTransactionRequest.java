91dcc7e467d94917f48254d647b5e45828097387
trong1605 <trongnl7145@ut.edu.vn>
2025-11-24 17:12:46 +0700

    Delete backend directory

--- backend/src/main/java/com/example/carboncredit/dto/ResolveTransactionRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.carboncredit.dto;
-
-import lombok.Data;
-
-@Data
-public class ResolveTransactionRequest {
-    private String action; // "refund" or "complete" or "adjust"
-    private String comment;
-}
--- backend/src/main/java/com/example/carboncredit/security/userDetailsService.java
+++
@@ -1,5 +0,0 @@
-package com.example.carboncredit.security;
-
-public class userDetailsService {
-
-}
